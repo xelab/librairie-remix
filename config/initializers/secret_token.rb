c7d23b33e694b7f10590cdf0942db1f230f0e3b6
Alex <lex.a@free.fr>
2013-07-08 21:44:53 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LibrairieRemix::Application.config.secret_key_base = '1ae855d6fcaede4a9632d9f7f6604d38e11b9035a81e73f61ff5da8549de774e6f99612111caaa4e7f9bcbb0184a141e885f1a5a55db6b667526f3a73318a316'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LibrairieRemix::Application.config.session_store :cookie_store, key: '_librairie-remix_session'
