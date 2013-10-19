225cbff38efea548df412607bb3b1af7c00c58f4
Alex <lex.a@free.fr>
2013-10-19 19:30:11 +0200

    config for prod, add postgresql gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ LibrairieRemix::Application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -60,6 +60,11 @@ LibrairieRemix::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+
+  config.assets.precompile += %w(
+    ng-select2.js underscore.js
+    portbox.css select2.css
+    )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
